8c53ff978b9e2bc284681f322093592423cbacb0
Anupam-dagar <dagaranupam@gmail.com>
2018-03-30 18:05:23 +0530

    change to material kit pro, change login frontend

--- library/views.py
+++ library/views.py
@@ -6,6 +6,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     if request.user.is_authenticated():
-        return HttpResponse("Hi")
+        return render(request, 'library/index.html', {})
     else:
         return redirect('accounts/login')
