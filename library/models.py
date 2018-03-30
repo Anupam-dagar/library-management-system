c053b88fe8d1a0229eeacca932f33aeae7340ae3
Anupam-dagar <dagaranupam@gmail.com>
2018-03-30 16:34:47 +0530

    add student and librarian table

--- library/models.py
+++ library/models.py
@@ -2,13 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Books(models.Model):
-    book_id = models.CharField(max_length=10, primary_key=True)    
-    title = models.CharField(max_length=200)
+    book_id = models.CharField(max_length=10, blank=False, primary_key=True)    
+    title = models.CharField(max_length=200, blank=False)
     author = models.ForeignKey('Author')
-    isbn = models.IntegerField()
+    isbn = models.IntegerField(blank=False)
     publisher = models.ForeignKey('Publisher')
 
     def __str__(self):
@@ -21,10 +22,10 @@ class Books(models.Model):
         ordering = ['title']
 
 class Author(models.Model):
-    firstname = models.CharField(max_length=200)
+    firstname = models.CharField(max_length=200, blank=False)
     middlename = models.CharField(max_length=200, blank=True, null=True)
-    lastname = models.CharField(max_length=200)
-    dob = models.DateField()
+    lastname = models.CharField(max_length=200, blank=False)
+    dob = models.DateField(blank=False)
 
     def __str__(self):
         return "Author " + self.firstname + " " + self.lastname
@@ -36,9 +37,9 @@ class Author(models.Model):
         ordering = ['lastname']
 
 class Publisher(models.Model):
-    name = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, blank=False)
+    country = models.CharField(max_length=200, blank=False)
+    city = models.CharField(max_length=200, blank=False)
 
     def __str__(self):
         return self.name + " " + self.country
@@ -48,3 +49,66 @@ class Publisher(models.Model):
 
     class Meta:
         ordering = ['name']
+
+class Student(models.Model):
+    male = 'Male'
+    female = 'Female'
+    it = 'IT'
+    ece = 'ECE'
+
+    GENDER_CHOICES = (
+        (male, 'Male'),
+        (female, 'Female'),
+    )
+
+    BRANCH_CHOICES = (
+        (it, 'IT'),
+        (ece, 'ECE'),
+    )
+
+    SEMESTER_CHOICES = (
+        (1, '1st'),
+        (2, '2nd'),
+        (3, '3rd'),
+        (4, '4th'),
+        (5, '5th'),
+        (6, '6th'),
+        (7, '7th'),
+        (8, '8th'),
+        (9, '9th'),
+        (10, '10th'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    enrollment_no = models.CharField(max_length=10, blank=False)
+    first_name = models.CharField(max_length=100, blank=False)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=False)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    department = models.CharField(max_length=10, choices=BRANCH_CHOICES)
+    semester = models.IntegerField(choices=SEMESTER_CHOICES)
+
+    def __str__(self):
+        return self.user.username + " " + self.enrollment_no
+
+    def __unicode__(self):
+        return self.user.username + " " + self.enrollment_no
+
+    class Meta:
+        ordering = ['semester']
+
+class Librarian(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=False)
+    middle_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=False)
+    librarian_id = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.user.username + " " + self.librarian_id
+
+    def __unicode__(self):
+        return self.user.username + " " + self.librarian_id
+
+    class Meta:
+        ordering = ['first_name']
