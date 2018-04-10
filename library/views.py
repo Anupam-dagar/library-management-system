8b0594c85073e11fc79d8710d51e625a31df77fe
Anupam-dagar <dagaranupam@gmail.com>
2018-04-10 20:38:16 +0530

    add query

--- library/views.py
+++ library/views.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Author, Books
 
 # Create your views here.
 def home(request):
     if request.user.is_authenticated():
+        try:
+            query = request.GET.get('q')
+        except ValueError:
+            query = None
+        if query:
+            detail = Books.objects.filter(author__firstname=query)
+            if not detail.exists():
+                detail = ['No results found!']
+            return render(request, 'library/index.html', {'detail': detail})
         return render(request, 'library/index.html', {})
     else:
         return redirect('accounts/login')
